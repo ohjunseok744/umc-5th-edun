43561bdb4d4cc96ec6b3a329d1f85eedf3a35aed
ohjunseok <ojspppp41@gmail.com>
2023-11-22 15:15:20 +0900

    mission8

---
+++ index.js
@@ -0,0 +1,34 @@
+import express from 'express';
+import { tempRouter } from './routes/temp.route.js';
+import { response } from './config/response.js';
+import { status } from './config/response.status.js';
+const app = express();
+const port = 3000;
+
+// router setting
+app.use('/temp', tempRouter);
+
+
+app.use((req, res, next) => {
+    const err = new BaseError(status.NOT_FOUND);
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+    console.log(err.data.status);
+    console.log(err.data.message);
+    // 템플릿 엔진 변수 설정
+    res.locals.message = err.message;   
+    // 개발환경이면 에러를 출력하고 아니면 출력하지 않기
+    res.locals.error = process.env.NODE_ENV !== 'production' ? err : {}; 
+    res.status(err.data.status).send(response(err.data));
+});
+
+app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`)
+});
+
+
+
+
+
